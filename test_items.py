a7876a9219af6595234ee3f76a14d028850e5cd6
repair-cat <simf61@yandex.ru>
2020-03-09 21:55:13 +0500

    a import was deleting

--- test_items.py
+++ test_items.py
@@ -2,4 +2,4 @@ import pytest
 from selenium import webdriver
 
 def test_find_button_of_basket(browser):
-    browser.find_element_by_class_name('btn')
+    assert browser.find_element_by_class_name('btn')
