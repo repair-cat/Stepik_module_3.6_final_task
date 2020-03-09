251c7f47ee0acc4efd422e8cac75793224959462
repair-cat <simf61@yandex.ru>
2020-03-09 21:52:35 +0500

    a import was deleting

--- conftest.py
+++ conftest.py
@@ -4,12 +4,12 @@ from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default='ru',
-                     help="Choose language: ru or es")
+                     help="Choose language")
 
 @pytest.fixture(scope="function")
 def browser(request):
     language = request.config.getoption('language')
-    print("\nopen site with Russian language..")
+    print(f"\nopen site with {language} language..")
     browser = webdriver.Chrome()
     link = f'http://selenium1py.pythonanywhere.com/{language}/catalogue/coders-at-work_207/' 
     browser.get(link)   
